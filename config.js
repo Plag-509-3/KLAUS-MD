a9b5882989a06ef7163a24e078192c2a04336de2
Dark1016 <roisandro86@gmail.com>
2024-09-19 11:53:14 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",
+SESSION_ID: process.env.SESSION_ID || "0CdXGAwb#lvhEA1BQvLiqDpntwsyAkA55ZFy0xm-S8BVtWT4cSig",
+MONGODB: process.env.MONGODB || " mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 };
